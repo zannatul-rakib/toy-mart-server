19849c8211253e1d26984fb080a1a05c55d5257d
zrakib-dev <dev.zrakib@gmail.com>
2023-07-06 20:01:54 +0600

    toys by id

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const toy = await toysCollection.findOne(query);
+      res.send(toy);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
